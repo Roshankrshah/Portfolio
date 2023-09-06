6ff28e7a4ff41a8de70f506e1cae293356c42eb3
Roshan <shahroshan0720@gmail.com>
2023-09-06 17:07:41 +0530

    side nav setup

--- script.js
+++ script.js
@@ -5,8 +5,16 @@ window.addEventListener("scroll",()=>{
 
 const menuBtn = document.querySelector('.menu-btn');
 const navigation = document.querySelector('.navigation');
+const navigationItems = document.querySelectorAll('.navigation a')
 
 menuBtn.addEventListener('click',()=>{
     menuBtn.classList.toggle('active');
     navigation.classList.toggle('active');
+});
+
+navigationItems.forEach(navigationItem=>{
+    navigationItem.addEventListener('click',()=>{
+        menuBtn.classList.remove('active');
+        navigation.classList.remove('active');
+    })
 })
