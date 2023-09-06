41291f3a9e315b002dd571637b8947ba523bce00
Roshan <shahroshan0720@gmail.com>
2023-09-06 15:43:07 +0530

    menu btn setup

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ window.addEventListener("scroll",()=>{
     const header = document.querySelector('header');
     header.classList.toggle('sticky',window.scrollY>0);
 })
+
+const menuBtn = document.querySelector('.menu-btn');
+const navigation = document.querySelector('.navigation');
+
+menuBtn.addEventListener('click',()=>{
+    menuBtn.classList.toggle('active');
+    navigation.classList.toggle('active');
+})
