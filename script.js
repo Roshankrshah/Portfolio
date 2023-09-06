3caf8e8df34270ed6b124ec103e591853805a5bc
Roshan <shahroshan0720@gmail.com>
2023-09-06 18:21:00 +0530

    prototype setup

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-window.addEventListener("scroll",()=>{
+window.addEventListener("scroll", () => {
     const header = document.querySelector('header');
-    header.classList.toggle('sticky',window.scrollY>0);
+    header.classList.toggle('sticky', window.scrollY > 0);
 })
 
 const menuBtn = document.querySelector('.menu-btn');
 const navigation = document.querySelector('.navigation');
 const navigationItems = document.querySelectorAll('.navigation a')
 
-menuBtn.addEventListener('click',()=>{
+menuBtn.addEventListener('click', () => {
     menuBtn.classList.toggle('active');
     navigation.classList.toggle('active');
 });
 
-navigationItems.forEach(navigationItem=>{
-    navigationItem.addEventListener('click',()=>{
+navigationItems.forEach(navigationItem => {
+    navigationItem.addEventListener('click', () => {
         menuBtn.classList.remove('active');
         navigation.classList.remove('active');
     });
@@ -22,11 +22,27 @@ navigationItems.forEach(navigationItem=>{
 
 const scrollBtn = document.querySelector('.scrollToTop-btn');
 
-window.addEventListener('scroll',()=>{
-    scrollBtn.classList.toggle('active',window.scrollY > 500)
+window.addEventListener('scroll', () => {
+    scrollBtn.classList.toggle('active', window.scrollY > 500)
 });
 
-scrollBtn.addEventListener('click',()=>{
+scrollBtn.addEventListener('click', () => {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
-})
+});
+
+window.addEventListener('scroll', reveal);
+
+function reveal() {
+    var reveals = document.querySelectorAll('.reveal');
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var revealTop = reveals[i].getBoundingClientRect().top;
+        var revealPoint = 50;
+
+        if (revealTop < windowHeight - revealPoint) {
+            reveals[i].classList.add('active');
+        }
+    }
+}
